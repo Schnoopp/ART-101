5d0caf72f288e3a5be60e0a95499ba0ec003057b
Schnoopp <quokadda@gmail.com>
2026-04-23 13:15:57 -0700

    button

--- excersises/ex04.js
+++ excersises/ex04.js
@@ -1,8 +1,43 @@
 let count = 0;
+let colorCount = -1;
+let colors = ["Orchid", "Coral", "Pink", "Plum", "Orange", "Olive", "cadetblue"];
 
-$("needy-button").click(function () {
+let myCat={
+    name: "Matroskin",
+    color: "Grey",
+    ears: 2,
+    hasKittens: true,
+    purr: function(){
 
-count = count+=1;
-$("needy-button").html("I've been clicked"+count+"times!")
+        console.log("purrrrr")
+    },
+    eat_sandwich: function(){
+
+        console.log(name +" you're eating the sandiwch wrong")
+    }
+
+}
+
+
+
+$("#needy-button").click(function () {
+
+    count = count + 1
+    colorCount = colorCount + 1
+
+    if (colorCount >= colors.length) {
+
+        colorCount = 0
+    }
+
+    $("#needy-button").html("I've been clicked " + count + " times! " + colors[colorCount])
+
+    $("#needy-button").css("background-color", colors[colorCount])
+
+    
+
+
 
 })
+
+
